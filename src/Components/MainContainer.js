ba9f71fd29545c7c2dcabcc2afb90ae94177982f
YashChawla030 <yashchawla030@gmail.com>
2023-04-21 22:52:36 +0530

    first commit

---
+++ src/Components/MainContainer.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import "../styles/MainContainer.css";
+import { FaUsers } from "react-icons/fa";
+import { AudioList } from "./AudioList";
+
+
+function MainContainer() {
+  useEffect(() => {
+    const allLi = document.querySelector(".menuList").querySelectorAll("li");
+
+    function changePopularActive() {
+      allLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    allLi.forEach((n) => n.addEventListener("click", changePopularActive));
+  }, []);
+
+  return (
+    <div className="mainContainer">
+     
+      <div className="menuList">
+        <ul>
+          <li>
+            <a>Popular Songs</a>
+          </li>
+         
+        </ul>
+
+       
+      </div>
+
+      <AudioList />
+    </div>
+  );
+}
+
+export { MainContainer };
