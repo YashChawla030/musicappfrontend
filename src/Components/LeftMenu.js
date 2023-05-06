aef3722c51f4084faa05972d3e6277af30566d4b
YashChawla030 <yashchawla030@gmail.com>
2023-05-07 00:14:42 +0530

    changes

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -5,9 +5,40 @@ import { BiSearchAlt } from "react-icons/bi";
 import { Menu } from "./Menu";
 import { MenuList } from "./MenuList";
 import { MenuPlayList } from "./MenuPlayList";
-
+import {FiLogOut} from 'react-icons/fi'
+import { useNavigate} from 'react-router-dom';
 
 function LeftMenu() {
+  const navigate = useNavigate();
+  const [name,setName] = React.useState(null);
+  const logout=() => {
+    navigate('/');
+  }
+  const searchsongs=() => {
+    async function makeRequest() {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/search/?song=${name}`,{headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST"})
+          
+    
+        console.log('response.status: ', response.status); 
+        console.log(response);
+        if(response.status===200){
+          console.log("we get data")
+          
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    
+    makeRequest();
+  
+    
+  }
   return (
     <div className="leftMenu">
       <div className="logoContainer">
@@ -17,15 +48,18 @@ function LeftMenu() {
           </i>
 
           <h2>Jaxx Music</h2>
+          <FiLogOut onClick={logout} style={{
+            marginLeft:60
+          }}/>
         </div>
       
       </div>
 
   
       <div className="searchBox">
-        <input type="text" placeholder="Search..." />
+          <input className='input-field' type={'text'} autoComplete={"searchsongs"} placeholder={'Search...'} onChange={e => setName(e.target.value)}/>
         <i>
-          <BiSearchAlt />
+          <BiSearchAlt onClick={searchsongs}/>
         </i>
       </div>
 
